0028c73cf99336a14737972b5a531df84b638290
tin duong <tinduong1809@gmail.com>
2023-07-26 13:28:24 -0500

    Update window manifest and window size

--- CivicMobile/App.xaml.cs
+++ CivicMobile/App.xaml.cs
@@ -5,7 +5,23 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-
         MainPage = new AppShell();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+#if WINDOWS
+        // todo currently Splashscreen does not work on window,
+        // create a story to implement window splashscreen
+        var width = 600;
+        var height = 800;
+
+        window.Width = width;
+        window.Height = height;
+#endif
+
+        return window;
     }
 }
